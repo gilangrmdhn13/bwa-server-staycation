12fd8fe817bcc5f4c5924928a9b1bad1e6a6f514
Gilang <gilangocon@gmail.com>
2020-11-11 14:07:03 +0700

    salah script cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors');
 
 // import mongoose
 const mongoose = require('mongoose');
@@ -21,7 +22,6 @@ var usersRouter = require('./routes/users');
 // router admin
 const adminRouter = require('./routes/admin');
 const apiRouter = require('./routes/api');
-const cors = require('cors');
 
 var app = express();
 
@@ -37,6 +37,7 @@ app.use(session({
   cookie: {maxAge: 60000}
 }));
 app.use(flash());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -48,7 +49,6 @@ app.use('/users', usersRouter);
 // admin
 app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
-app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
