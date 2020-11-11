257f6595797341a7532a98ce0c04b40b6a80997e
Gilang <gilangocon@gmail.com>
2020-11-11 13:58:53 +0700

    salah penempatan script cors

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use('/users', usersRouter);
 // admin
 app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
-app.use(cors());
+const cors = require('cors');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
